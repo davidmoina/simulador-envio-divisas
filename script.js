5a04287d561d0585db6932b1127d06ac65e169b9
davidmoina <luisdavidmoina@gmail.com>
2022-07-05 23:54:00 +0200

    primer desafio obligatorio terminado

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-let desafios = prompt("¿Cuantos desafios tiene este curso de Javascript en Coderhouse? Introduce fin para finalizar el bucle si no logras acertarlo.");
+const total = (num1, num2) => num1 - num2;
+const conversion = (num1, num2) => num1 * num2;
 
-while(desafios != 11 && desafios != "fin") {
-  alert("Incorrecto, intentalo de nuevo.");
 
-  desafios = prompt("Ingresa otro numero. Introduce fin para finalizar el bucle si no logras acertarlo.");
-};
+let nombre, nombreDestinatario, dinero, impuesto, divisa;
+const euro = 0.007;
+const peso = 129.46;
+const comision = 0.05;
+
+alert("Enviamos dinero entre España y Argentina, gracias por usar nuestro servicio. Pulse en Aceptar para continuar.");
+nombre = prompt("Ingrese su nombre.");
+nombreDestinatario = prompt("Ingrese el nombre del destinatario");
+
+
+do {
+  pais = prompt("A que pais desea enviar el dinero. España o Argentina.").toLowerCase();
+
+
+if(pais === "españa") {
+  divisa = "pesos argentinos"
+} else if(pais === "argentina") {
+  divisa = "euros"
+} else {
+  alert("Ingrese un pais valido");
+}
+} while(pais != "españa" && pais != "argentina" )
+
+
+do {
+  dinero = parseFloat(prompt(`Ingrese la cantidad en ${divisa} a enviar.`));
+
+  if(isNaN(dinero)) {
+    alert("Por favor ingrese numeros validos");
+  };
+
+} while(isNaN(dinero))
+
+let impuestoDescontar = conversion(dinero, comision);
+impuesto = prompt(`${nombre}, la conversion de divisa le costara ${impuestoDescontar} ${divisa}, ¿desea continuar? Si o No.`).toLowerCase();
+
+let dineroTotal = total(dinero, impuestoDescontar);
+
+if(impuesto === "si" && pais === "españa") {
+  dineroFinal = conversion(dineroTotal, euro);
+  alert( `Muchas gracias por usar nuestro servicio, le llegara a ${nombreDestinatario} en España la cantidad de ${dineroFinal} euros.`);
+
+} else if(impuesto === "si" && pais === "argentina") {
+  dineroFinal = conversion(dineroTotal, peso);
+  alert(`Muchas gracias por usar nuestro servicio, le llegara a ${nombreDestinatario} en Argentina la cantidad de ${dineroFinal} pesos argentinos.`);
+} else {
+  alert("Muchas gracias por su tiempo");
+}
