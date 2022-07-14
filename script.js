3feb244da96248aa0d9d95f4e5849173dcdd0efe
davidmoina <luisdavidmoina@gmail.com>
2022-07-14 20:47:37 +0200

    desafio obligatorio terminado #4

--- script.js
+++ script.js
@@ -1,60 +1,6 @@
 const total = (num1, num2) => num1 - num2;
 const conversion = (num1, num2) => num1 * num2;
 
-
-let nombre, nombreDestinatario, dinero, impuesto, divisa;
-const euro = 0.007;
-const peso = 129.46;
-const comision = 0.05;
-
-alert("Enviamos dinero entre España y Argentina, gracias por usar nuestro servicio. Pulse en Aceptar para continuar.");
-nombre = prompt("Ingrese su nombre.");
-nombreDestinatario = prompt("Ingrese el nombre del destinatario");
-
-
-do {
-  pais = prompt("A que pais desea enviar el dinero. España o Argentina.").toLowerCase();
-
-
-if(pais === "españa") {
-  divisa = "pesos argentinos"
-} else if(pais === "argentina") {
-  divisa = "euros"
-} else {
-  alert("Ingrese un pais valido");
-}
-} while(pais != "españa" && pais != "argentina" )
-
-
-do {
-  dinero = parseFloat(prompt(`Ingrese la cantidad en ${divisa} a enviar.`));
-
-  if(isNaN(dinero)) {
-    alert("Por favor ingrese numeros validos");
-  };
-
-} while(isNaN(dinero))
-
-let impuestoDescontar = conversion(dinero, comision);
-
-do {
-  impuesto = prompt(`${nombre}, la conversion de divisa le costara ${impuestoDescontar} ${divisa}, ¿desea continuar? Si o No.`).toLowerCase();
-} while(impuesto != "si" && impuesto != "no")
-
-let dineroTotal = total(dinero, impuestoDescontar);
-
-if(impuesto === "si" && pais === "españa") {
-  dineroFinal = conversion(dineroTotal, euro);
-  alert( `Muchas gracias por usar nuestro servicio, le llegara a ${nombreDestinatario} en España la cantidad de ${dineroFinal} euros.`);
-
-} else if(impuesto === "si" && pais === "argentina") {
-  dineroFinal = conversion(dineroTotal, peso);
-  alert(`Muchas gracias por usar nuestro servicio, le llegara a ${nombreDestinatario} en Argentina la cantidad de ${dineroFinal} pesos argentinos.`);
-} else {
-  dineroFinal = 0;
-  alert("Muchas gracias por su tiempo");
-}
-
 class Envios {
   constructor(remitente, destinatario, destino, cantidad, comision, cambio) {
     this.remitente = remitente;
@@ -67,8 +13,74 @@ class Envios {
 }
 
 const movimientos = [];
-const movimiento1 = new Envios(nombre, nombreDestinatario, pais, dinero, impuestoDescontar, dineroFinal);
-movimientos.push(movimiento1);
+
+const euro = 0.007;
+const peso = 129.46;
+const comision = 0.05;
+
+let nombre, nombreDestinatario, dinero, impuesto, divisa, pais, continuar, impuestoDescontar;
+
+alert("Enviamos dinero entre España y Argentina, gracias por usar nuestro servicio. Pulse en Aceptar para continuar.");
+
+//solicitamos datos al usuario y a la vez los vamos validando
+do {
+  do {
+    nombre = prompt("Ingrese su nombre.");
+    nombreDestinatario = prompt("Ingrese el nombre del destinatario");
+  
+  } while(nombre.length == 0 || nombreDestinatario.length == 0);
+  
+  do {
+    pais = prompt("A que pais desea enviar el dinero. España o Argentina.").toLowerCase();
+  
+  
+  if(pais === "españa") {
+    divisa = "pesos argentinos"
+  } else if(pais === "argentina") {
+    divisa = "euros"
+  } else {
+    alert("Ingrese un pais valido");
+  }
+  } while(pais != "españa" && pais != "argentina" )
+  
+  
+  do {
+    dinero = parseFloat(prompt(`Ingrese la cantidad en ${divisa} a enviar.`));
+  
+    if(isNaN(dinero) || dinero <= 0) {
+      alert("Por favor ingrese numeros validos");
+    };
+  
+  } while(isNaN(dinero) || dinero <= 0)
+  
+  let impuestoDescontar = conversion(dinero, comision);
+  
+  do {
+    impuesto = prompt(`${nombre}, la conversion de divisa le costara ${impuestoDescontar} ${divisa}, ¿desea continuar? Si/No.`).toLowerCase();
+  } while(impuesto != "si" && impuesto != "no")
+  
+  let dineroTotal = total(dinero, impuestoDescontar);
+  
+  if(impuesto === "si" && pais === "españa") {
+    dineroFinal = conversion(dineroTotal, euro);
+    alert( `Muchas gracias por usar nuestro servicio, le llegara a ${nombreDestinatario} en España la cantidad de ${dineroFinal} euros.`);
+  
+  } else if(impuesto === "si" && pais === "argentina") {
+    dineroFinal = conversion(dineroTotal, peso);
+    alert(`Muchas gracias por usar nuestro servicio, le llegara a ${nombreDestinatario} en Argentina la cantidad de ${dineroFinal} pesos argentinos.`);
+  } else {
+    dineroFinal = 0;
+    alert("Muchas gracias por su tiempo");
+  }
+
+  const movimiento = new Envios(nombre, nombreDestinatario, pais, dinero, impuestoDescontar, dineroFinal);
+  movimientos.push(movimiento);
+
+  do {
+    continuar = prompt("¿Desea hacer un nuevo envio? Si/No.").toLowerCase();
+
+  } while(continuar != "si" && continuar != "no")
+} while(continuar != "no")
 
 console.log(movimientos);
 
