ae54e1436ddf455fec5d833c02003d346b15c300
davidmoina <luisdavidmoina@gmail.com>
2022-08-04 01:02:20 +0200

    agregando funcionalidad y librerias #10

--- script.js
+++ script.js
@@ -28,11 +28,14 @@ botonLightMode.addEventListener("click", () => {
 const total = (num1, num2) => num1 - num2;
 const conversion = (num1, num2) => num1 * num2;
 
+
+
 //clase para hacer los arrays de movimientos
 class Envios {
-  constructor(remitente, destinatario, destino, cantidad, comision, cambio) {
+  constructor(remitente, destinatario, origen, destino, cantidad, comision, cambio) {
     this.remitente = remitente;
     this.destinatario = destinatario;
+    this.origen = origen;
     this.destino = destino;
     this.cantidad = cantidad;
     this.comision = comision;
@@ -40,28 +43,41 @@ class Envios {
   }
 }
 
-const euro = 0.007;
-const peso = 129.46;
+const euroPeso = 134.66;
+const euroDolar = 1.05;
+const pesoEuro = 0.007;
+const pesoDolar = 0.008;
+const dolarEuro = 0.98;
+const dolarPeso = 132.40;
 const comision = 0.05;
 
 let nombre, nombreDestinatario, impuesto, divisa, continuar, dineroFinal, impuestoDescontar;
 
-let pais = document.getElementById("idPais");
-let textoDinero = document.getElementById("textoDinero");
+//funcion para seleccionar el tipo de divisa segun el pais
+function divisaFuncion (country) {
+
+  if(country === "España") {
+    divisa = "euros"
+  } else if(country === "Argentina") {
+    divisa = "pesos argentinos"
+  } else if(country === "Estados Unidos") {
+    divisa = "dolares"
+  };
+};
 
 //aqui segun seleccione el pais en el input de debajo se cambiara el texto entre pesos o euros segun interactue el usuario
-pais.addEventListener("change", () => {
+let paisOrigen = document.getElementById("idPaisOrigen");
+let textoDinero = document.getElementById("textoDinero");
 
-  if(pais.value === "españa") {
-    divisa = "pesos argentinos"
-  } else if(pais.value === "argentina") {
-    divisa = "euros"
-  }
-
+paisOrigen.addEventListener("change", () => {
+  //llamamos la funcion anterior para seleccionar la divisa
+  divisaFuncion(paisOrigen.value);
   textoDinero.innerHTML = `
     Ingrese la cantidad en ${divisa} a enviar:
   `
 });
+
+let paisDestino = document.getElementById("idPaisDestino");
 
 let dinero = document.getElementById("idDinero");
 let divComision = document.getElementById("divComision");
@@ -70,25 +86,42 @@ let divComision = document.getElementById("divComision");
 //agregamos el texto debajo de la cantidad de dinero segun el pais y moneda
 dinero.addEventListener("input", () => {
 
-  if(pais.value === "españa") {
-    divisa = "pesos argentinos"
-  } else if(pais.value === "argentina") {
-    divisa = "euros"
-  }
+  divisaFuncion(paisOrigen.value);
+  console.log(divisa);
 
+  //llamamos la funcion flecha para descontar el impuesto
   impuestoDescontar = parseFloat(conversion(dinero.value, comision).toFixed(2));
 
   let dineroTotal = total(dinero.value, impuestoDescontar);
-    
-  if(pais.value === "españa") {
-    dineroFinal = conversion(dineroTotal, euro);
 
-  } else if(pais.value === "argentina") {
-    dineroFinal = conversion(dineroTotal, peso);
-  } else {
-    dineroFinal = 0;
+  if(paisOrigen.value == paisDestino.value) {
+    dineroFinal = conversion(dineroTotal, 1);
+  }
+    
+  if(paisOrigen.value == "España" && paisDestino.value == "Argentina") {
+    dineroFinal = conversion(dineroTotal, euroPeso);
+  }
+  
+  if(paisOrigen.value == "España" && paisDestino.value == "Estados Unidos") {
+    dineroFinal = conversion(dineroTotal, euroDolar);
   }
 
+  if(paisOrigen.value == "Argentina" && paisDestino.value == "España") {
+    dineroFinal = conversion(dineroTotal, pesoEuro);
+  }
+
+  if(paisOrigen.value == "Argentina" && paisDestino.value == "Estados Unidos") {
+    dineroFinal = conversion(dineroTotal, pesoDolar);
+  }
+
+  if(paisOrigen.value == "Estados Unidos" && paisDestino.value == "España") {
+    dineroFinal = conversion(dineroTotal, dolarEuro);
+  }
+
+  if(paisOrigen.value == "Estados Unidos" && paisDestino.value == "Argentina") {
+    dineroFinal = conversion(dineroTotal, dolarPeso);
+  }
+  
   divComision.innerHTML = `
       <p>La conversion de divisa le costara ${impuestoDescontar} ${divisa}</p>
     `
@@ -113,12 +146,25 @@ form.addEventListener("submit", (event) => {
   nombre = document.getElementById("idNombre").value;
   nombreDestinatario = document.getElementById("idDestinatario").value;
   
-  const movimiento = new Envios(nombre, nombreDestinatario, pais.value, dinero.value, impuestoDescontar, dineroFinal);
+  const movimiento = new Envios(nombre, nombreDestinatario,paisOrigen.value, paisDestino.value, dinero.value, impuestoDescontar, dineroFinal);
   movimientos.push(movimiento);
 
   localStorage.setItem("movimientoStorage", JSON.stringify(movimientos));
 
   divComision.innerHTML = "";
+
+  Toastify({
+    text: "Dinero enviado correctamente",
+    duration: 3000,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();
 
   form.reset();
 });
@@ -141,11 +187,15 @@ botonMostrar.addEventListener("click", () => {
         <h3>Movimiento ${index}</h3>
         <p>Nombre: ${giro.remitente}</p>
         <p>Destinatario: ${giro.destinatario}</p>
+        <p>Pais Origen: ${giro.origen}</p>
         <p>Pais Destino: ${giro.destino}</p>
+        <p>Dinero Enviado: ${giro.cantidad}</p>
+        <p>Comision: ${giro.comision}</p>
         <p>Dinero Ingresado: ${giro.cambio}</p>
-        <button class="botonEliminar">Eliminar Tarea</button>
+        <button class="botonEliminar">Eliminar Registro</button>
       </div>
     `;
+
   });
 
   //consultamos y le doy funcionalidad al boton para eliminar cierto movimiento tanto del array, dom y localstorage y muestro el movimiento eliminado en storage
@@ -154,9 +204,10 @@ botonMostrar.addEventListener("click", () => {
     let botonCard = document.getElementById(`movimiento${index}`).lastElementChild;
     botonCard.addEventListener("click", () => {
       document.getElementById(`movimiento${index}`).remove();
-      movimientos.splice(index, 1);
+      movimientos.splice(giro, 1);
       localStorage.setItem("movimientoStorage", JSON.stringify(movimientos));
       console.log(`Movimiento de ${giro.remitente} eliminado correctamente.`);
+
     });
   });
 
